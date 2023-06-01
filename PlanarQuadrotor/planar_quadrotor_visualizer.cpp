ab6a76184af967f33d57f17fb8524c7a1598c329
aliskiewicz03 <114615874+aliskiewicz03@users.noreply.github.com>
2023-06-01 20:38:10 +0200

    Update planar_quadrotor_visualizer.cpp

--- PlanarQuadrotor/planar_quadrotor_visualizer.cpp
+++ PlanarQuadrotor/planar_quadrotor_visualizer.cpp
@@ -10,13 +10,27 @@ PlanarQuadrotorVisualizer::PlanarQuadrotorVisualizer(PlanarQuadrotor *quadrotor_
  */
 void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer> &gRenderer) {
     Eigen::VectorXf state = quadrotor_ptr->GetState();
-    float q_x, q_y, q_theta;
-
+    float q_x, q_y, q_theta, x1, x2, y1, y2;
+    const int l = 160, h = 30;
+    Sint16 v_x[4];
+    Sint16 v_y[4];
     /* x, y, theta coordinates */
     q_x = state[0];
     q_y = state[1];
     q_theta = state[2];
+    x1 = q_x + cos(q_theta)*(l/2);
+    x2 = q_x - cos(q_theta)*(l/2);
+    y1 = q_y - sin(q_theta)*(l/2);
+    y2 = q_y + sin(q_theta)*(l/2);
+    v_x[0] = x1 - sin(q_theta)*(h/2);
+    v_x[1] = x1 + sin(q_theta)*(h/2);
+    v_x[2] = x2 + sin(q_theta)*(h/2);
+    v_x[3] = x2 - sin(q_theta)*(h/2);
+    v_y[0] = y1 - cos(q_theta)*(h/2);
+    v_y[1] = y1 + cos(q_theta)*(h/2);
+    v_y[2] = y2 + cos(q_theta)*(h/2);
+    v_y[3] = y2 - cos(q_theta)*(h/2);
 
-    SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0x00, 0x00, 0xFF);
-    filledCircleColor(gRenderer.get(), q_x, q_y, 30, 0xFF0000FF);
+    SDL_SetRenderDrawColor(gRenderer.get(), 0xF5, 0x28, 0x91, 0xCC);
+    filledPolygonColor(gRenderer.get(), v_x, v_y, 4, 0xF52891CC);
 }
